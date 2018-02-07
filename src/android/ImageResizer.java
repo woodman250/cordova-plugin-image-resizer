074351d04b97382b9036a95609f133724f1c8763
ironskinspirit <dds810818@126.com>
2018-02-07 17:57:33 +0800

    add FileHelper.java from cordova-plugin-camera

--- src/android/ImageResizer.java
+++ src/android/ImageResizer.java
@@ -9,7 +9,7 @@ import android.util.Log;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
-import org.apache.cordova.camera.FileHelper;
+import info.protonet.imageresizer.FileHelper;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
